692589b546b2891da6d6cf3fc05b31690d62caf5
benilies <iliesbenh@gmail.com>
2024-08-19 15:58:49 +0300

    hotfix https error certificate generation

--- resources/utils.py
+++ resources/utils.py
@@ -1,6 +1,6 @@
 def get_api_base_url(host, port):
     # Determine the scheme (HTTP or HTTPS) based on the port number.
-    scheme = "https" if port == 443 else "http"
+    scheme = "https" if port == "443" else "http"
 
     # Construct the full API URL with the determined scheme and port.
     api_url = f"{scheme}://{host}:{port}/"
