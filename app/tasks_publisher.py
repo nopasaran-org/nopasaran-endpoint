aad6c57716af87ec6eea8646d5a181e77359fbf0
benilies <iliesbenh@gmail.com>
2024-09-17 09:09:46 +0300

    test concurrency control

--- app/tasks_publisher.py
+++ app/tasks_publisher.py
@@ -16,17 +16,27 @@ def init_publisher():
     os.makedirs(inputs_dir, exist_ok=True)
     os.makedirs(results_dir, exist_ok=True)
     
+def complete_task_variables(variables, task_id):
+    # Get the top-level key (it could be any, so we just take the first one)
+    top_key = next(iter(variables))
+    
+    # Iterate over each worker in the top-level key
+    for _, worker_data in variables[top_key].items():
+        # Add the new key-value pairs to each worker
+        worker_data['id'] = task_id
+        worker_data['signaling-server'] = get_netbird_ip()
+        worker_data['signaling-port'] = 1963
+
+    return variables    
+    
 def publish_task(task_id, repository, tests_tree, nodes, variables):
-    variables['id'] = task_id
-    variables['signaling-server'] = get_netbird_ip()
-    variables['signaling-port'] = 1963
     try:
         task = {
             "id": task_id,
             "repository": repository,
             "tests_tree": tests_tree,
             "nodes": nodes,
-            "variables": variables
+            "variables": complete_task_variables(variables, task_id)
         }
         task_file = os.path.join(inputs_dir, f"task_{task_id}.json")
         with open(task_file, "w") as file:
