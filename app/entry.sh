372b0e126f4fe8f2ecce725eea7351c1e5769cfc
benilies <iliesbenh@gmail.com>
2025-02-09 20:05:01 +0300

    add token for authentication with github for rate limiting

--- app/entry.sh
+++ app/entry.sh
@@ -4,6 +4,9 @@ netbird service start
 pip install --upgrade pip
 pip install --upgrade nopasaran
 
+git config --global credential.helper store
+echo "https://$GITHUB_TOKEN@github.com" > ~/.git-credentials
+
 python /app/iptables/iptables_helper.py
 python /app/tasks_consumer.py & disown
 
